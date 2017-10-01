62693f0d0481844969399f674158c62cfcaf790a
Joonki <jkkim@nextree.co.kr>
2017-10-01 21:41:08 +0900

    update musics view & search-modal

--- kongnamul/www/views/musics/musics.js
+++ kongnamul/www/views/musics/musics.js
@@ -20,13 +20,23 @@ angular.module('App')
   
   $scope.showSearchModal = function (condition) {
 	  $scope.conditions = condition;
+	  $scope.beforeConditions = condition;
+	  $scope.conditionKey = condition.englishName;
 	  switch(condition.englishName) {
-	  	case 'composers' : $scope.conditions = $scope.composers; break;
-	  	case 'genres' : $scope.conditions = $scope.genres; break;
-	  	case 'instruments' : $scope.conditions = $scope.instruments; break;
+	  	case 'composers' : $scope.conditions = $scope.composers;
+	  	                   $scope.beforeConditions = angular.copy($scope.composers);
+	  	                   $scope.conditionName = '작곡가';
+	  	                   break;
+	  	case 'genres' : $scope.conditions = $scope.genres;
+	                    $scope.beforeConditions = angular.copy($scope.genres);
+	  	                $scope.conditionName = '장르';
+	  	                break;
+	  	case 'instruments' : $scope.conditions = $scope.instruments;
+	                         $scope.beforeConditions = angular.copy($scope.instruments);
+	  	                     $scope.conditionName = '악기';
+	  	                     break;
 	  	default : break;
 	  }
-      console.log($scope.conditions[0].koreanName);
       if ($scope.searchModal) {
   		  $scope.searchModal.show();
   	  } else {
@@ -39,8 +49,53 @@ angular.module('App')
   	  }
   };
     
-  $scope.hideSearchModal = function () {
+  $scope.hideSearchModal = function (canceled) {
+	  if (canceled) {
+		  switch($scope.conditionKey) {
+		    case 'composers' : $scope.composers = angular.copy($scope.beforeConditions); break;
+		    case 'genres' : $scope.genres = angular.copy($scope.beforeConditions); break;
+		    case 'instruments' : $scope.instruments = angular.copy($scope.beforeConditions); break;
+		    default : break;
+		  }
+	  }
       $scope.searchModal.hide();
+  };
+  
+  $scope.checkAllSelected = function(condition) {
+	  var conditions = $scope.conditions;
+	  var selectedCount = 0;
+
+	  if ('All' == condition.englishName) {
+		  if (conditions[0].selected) {
+			  for (var i = 1; i < conditions.length; i++) {
+				  conditions[i].selected = true;
+			  }
+		  } else {
+			  for (var i = 1; i < conditions.length; i++) {
+				  if (conditions[i].selected) {
+					  selectedCount++
+				  }
+			  }
+			  if (selectedCount == conditions.length - 1) {
+				  for (var i = 1; i < conditions.length; i++) {
+					  conditions[i].selected = false;
+				  } 
+			  }
+		  }
+	  } else {
+		  if (conditions[0].selected) {
+			  conditions[0].selected = false;
+		  } else {
+			  for (var i = 1; i < conditions.length; i++) {
+				  if (conditions[i].selected) {
+					  selectedCount++
+				  }
+			  }
+			  if (selectedCount == conditions.length - 1) {
+				  conditions[0].selected = true;
+			  }
+		  }
+	  }
   };
 
   $scope.load();
