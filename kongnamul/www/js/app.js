386e93d4636923b488eab06b0014d2d09e11362d
Joonki <jkkim@nextree.co.kr>
2017-10-18 00:29:43 +0900

    local storage setting (ing...)

--- kongnamul/www/js/app.js
+++ kongnamul/www/js/app.js
@@ -88,21 +88,60 @@ angular.module('App', ['ionic'])
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+    if(!$localstorage.get("musicSearchConditions")) {
+    	$localstorage.set("musicSearchConditions", function() {
+    		 return {
+    		     Composers:
+    		     	 [{ englishName: 'All', koreanName: '모든 작곡가', selected: true },
+    	    	      { englishName: 'Bach', koreanName: '바하', selected: false },
+    	    		  { englishName: 'Beethoven', koreanName: '베토벤', selected: false },
+    		     	  { englishName: 'Chopin', koreanName: '쇼팽', selected: false }],
+    		     Genres:
+    		     	 [{ englishName: 'All', koreanName: '모든 장르', selected: true },
+    		          { englishName: 'Symphony', koreanName: '교향곡', selected: false },
+    		     	  { englishName: 'Concerto', koreanName: '협주곡', selected: false }],
+    		     Instruments:
+    		     	 [{ englishName: 'All', koreanName: '모든 악기', selected: true },
+    		     	  { englishName: 'Orchestra', koreanName: '오케스트라', selected: false },
+    		     	  { englishName: 'Piano', koreanName: '피아노', selected: false }]
+    		 };
+    	});
+    }
   });
 })
+
+.factory('$localstorage', ['$window', function ($window) {
+	  return {
+	      set: function (key, value) {
+	          $window.localStorage[key] = value;
+	      },
+	      get: function (key, defaultValue) {
+	          return $window.localStorage[key] || defaultValue;
+	      },
+	      setObject: function (key, value) {
+	          $window.localStorage[key] = JSON.stringify(value);
+	      },
+	      getObject: function (key) {
+	          return JSON.parse($window.localStorage[key] || '{}');
+	      },
+	      remove: function(key) {
+	          $window.localStorage.removeItem(key);
+	      }
+	  }
+}])
 
 .factory('Conditions', function() {
 	 return {
 	     Composers:
 	     	 [{ englishName: 'All', koreanName: '모든 작곡가', selected: true },
-     	      { englishName: 'Bach', koreanName: '바하', selected: false },
-     		  { englishName: 'Beethoven', koreanName: '베토벤', selected: false },
+    	      { englishName: 'Bach', koreanName: '바하', selected: false },
+    		  { englishName: 'Beethoven', koreanName: '베토벤', selected: false },
 	     	  { englishName: 'Chopin', koreanName: '쇼팽', selected: false }],
 	     Genres:
 	     	 [{ englishName: 'All', koreanName: '모든 장르', selected: true },
 	          { englishName: 'Symphony', koreanName: '교향곡', selected: false },
 	     	  { englishName: 'Concerto', koreanName: '협주곡', selected: false }],
- 	     Instruments:
+	     Instruments:
 	     	 [{ englishName: 'All', koreanName: '모든 악기', selected: true },
 	     	  { englishName: 'Orchestra', koreanName: '오케스트라', selected: false },
 	     	  { englishName: 'Piano', koreanName: '피아노', selected: false }]
